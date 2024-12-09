130412ed14fe683ec67556d8e8ebc900c1869891
AnzhelikaZhalynbekova <anjiko333@gmail.com>
2024-12-09 13:04:23 +0600

    Whole code of the project

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import Blackjack.Blackjack;
+
+public class Main {
+    public static void main(String[] args){
+        Blackjack blackjack = new Blackjack();
+        blackjack.run();
+        //main class for compilaton
+    }
+}
